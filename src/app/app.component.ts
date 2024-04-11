2cde95fda69edd66f8bfeea3a0ffb2f3ad010ce3
khalifaboulbayem <khalifa.boulbayem.eviden@gmail.com>
2024-04-11 14:59:16 +0100

    tarea de calculadora terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadura-app';
+  title = 'Calculadora (Tarea 2.3.4)';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  //Metodo sumar.
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+  //metodo restar
+  restar() : void{
+    this.resultado = this.numero1 - this.numero2;
+  }
+  //metodo multiplicar
+  multiplicar() : void{
+    this.resultado = this.numero1 * this.numero2;
+  }
+  //metodo dividir
+  dividir() : void{
+    this.resultado = this.numero1 / this.numero2;
+  }
+
 }
